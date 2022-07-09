a0b2899aa02e2df168d3352e6da568cbb14c6d5b
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-07-09 11:20:13 +0530

    chapter - 2 operators

---
+++ operators.py
@@ -0,0 +1,58 @@
+# Basic operations using operators
+a=10.0
+b=2
+print('a+b =',a+b)
+print('a-b =',a-b)
+print('a*b =',a*b)
+print('a/b =',a/b)
+print('a%b =',a%b)
+print('a//b =',a//b)
+print('a**b =',a**b)
+
+#using + operators as concatenation operators
+#compulsorly both the arguments should be string else type cast to string
+
+random = 'nilesh_'+'got_'+'the_'+'job_'+'in_'+'oct_'+str(2022)
+print(random)
+
+print("\n")
+#string repetetion
+proposal='I L U '*3
+print(proposal)
+
+print("\n")
+#Relational Operators
+print('a<b=',a<b)
+print('a<=b=',a<=b)
+print('a>b=',a>b)
+print('a>=b=',a>=b)
+
+#Logical operators
+print("\n")
+print(10 and 20)
+print([] and ())
+print(() or {10})
+print('' or 'nilesh')
+print(not 'negative')
+print(not '')
+print(not [])
+print(not 10,)
+
+#Logical operators
+print("\n")
+print(4&5)
+print(4|5)
+print(4^5)
+print(~(4))
+print(~(-4))
+print(~(-5))
+print(~(5))
+print(~(7))
+print(~(-7))
+
+#SHift operators
+print("\n")
+print(-10>>3)
+print(-8>>2)
+print(6>>3)
+print(-8>>3)
