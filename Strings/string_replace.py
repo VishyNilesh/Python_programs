0a1bb93655d220a08ec325d6abf800b9718e3148
Nilesh Kumar <nileshkumar.v21@gmail.com>
2022-08-10 15:43:42 +0530

    7. Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string.

---
+++ Strings/string_replace.py
@@ -0,0 +1,4 @@
+#7. Write a Python program to find the first appearance of the substring 'not' and 'poor' from a given string, 
+# if 'not' follows the 'poor', replace the whole 'not'...'poor' substring with 'good'. Return the resulting string.
+s = input('Enter the string ')
+print(s.find('not','poor'))
