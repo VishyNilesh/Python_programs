f1de2638c6599bbb59ace4b7da70315a9c9fe3fa
Nilesh Kumar <nileshkumar.v21@gmail.com>
2022-08-10 18:01:38 +0530

    to_upper&lower

---
+++ Strings/to_upper&lower.py
@@ -0,0 +1,4 @@
+#13. Write a Python program to count the occurrences of each word in a given sentence
+string = input('Enter the string to change the case ')
+print(string.upper())
+print(string.lower())
