399392dc73a72e3789d27841f1d9c0dd367a4837
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-09-05 15:56:20 +0530

    convert to list of words.py

---
+++ Strings/convert to list of words.py
@@ -0,0 +1,6 @@
+''' 46. Write a Python program to convert a given string into a list of words. Go to the editor
+Sample Output:
+['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.']
+['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.'] '''
+s =input ('Enter the string : ')
+print (s.split())
